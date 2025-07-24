1b499d4916e234f7c2349410baef5fee17984bd5
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-07-24 09:52:30 -0600

    commit prueba de trabajo en clase

--- Actividad_6.py
+++ Actividad_6.py
@@ -1,1 +1,49 @@
 #activivdad 6 del dia jueves
+def bienvenida():
+    print("____Bienvenido al sistema de operaciones matematicas___")
+
+def suma_total(a,b):
+    return a+b
+
+def premedio(a,b):
+    return a*b
+def numeros_positivos_negativos (a,b):
+    return
+
+def area_triangulo (a,b):
+    return 1/2*a*b
+
+def par_impar(numero):
+    return numero % 2 == 0
+
+def promedio_ncalificaciones (calificacion):
+    return calificacion+calificacion/calificacion
+
+def numero_mayor_menor (numeros):
+    return numeros
+
+bienvenida()
+
+def menu():
+    print("1. la suma total: ")
+    print("2. promedio: ")
+    print("3. La cantidad de numeros positivos y negativos")
+
+def menu2():
+    print("_____Menú de opcione_____")
+    print("1. Ingresar n numeros y mostrar:")
+    print("2. Calcular el area de un triangulo:")
+    print("3. Verificar si un numero es par o impar")
+    print("4. Calcular el promedio de n calificaciones: ")
+    print("5. Ingresar n numeros y mostrar el mayor y el menor")
+    print("6. Salir del programa")
+
+
+while True:
+    print (menu2)
+    opcion= int(input("Elija su opcion (1-6): "))
+
+    match case:
+        case "1":
+
+
