752bb9f6a224b6be87fa177d6b8006cb8da20b9e
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-08-12 11:26:06 -0600

    9 commit

--- Actividad_6.py
+++ Actividad_6.py
@@ -1,11 +1,11 @@
 #activivdad 6 del dia jueves
-def bienvenida():
+def bienvenida(): #funcion basica de bienvenida
     print("____Bienvenido al sistema de operaciones matematicas___")
 
 def suma_total(numeros): #funcion de suma total
     return sum(numeros)
 
-def premedio(numeros): #funcion del promedio de n numeros
+def promedio(numeros): #funcion del promedio de n numeros
     return sum(numeros)/ len(numeros)
 
 def numeros_positivos_negativos (numeros):
@@ -16,8 +16,8 @@ def numeros_positivos_negativos (numeros):
             numeros_nega +=1
         else:
             numeros_pos +=1
-print(f"TOTAL NUMEROS NEGATIVOS: {numeros_nega}")
-print(f"TOTAL NUMEROS POSITIVOS: {numeros_pos}")
+    print(f"TOTAL NUMEROS NEGATIVOS: {numeros_nega}")
+    print(f"TOTAL NUMEROS POSITIVOS: {numeros_pos}")
 
 
 def area_triangulo (base,altura): #funcion calcular area de un triangulo
@@ -25,9 +25,9 @@ def area_triangulo (base,altura): #funcion calcular area de un triangulo
 
 def par_impar(numero): # funcion par o impar
     if numero % 2 ==0:
-        print(f"{numero} es par")
+        print(f"{numero} = es par")
     else:
-        print(f"{numero} es impar")
+        print(f"{numero} = es impar")
 
 def promedio_n_notas(numeros): #funcion calcular el promedio de n notas
     while True:
@@ -36,8 +36,8 @@ def promedio_n_notas(numeros): #funcion calcular el promedio de n notas
             notas=int(input(f"Ingrese el numero_{i+1}: "))
             numeros.append(notas)
             print("Numero guardado")
-    promedio_n_notas= sum(numeros)/len(numeros)
-    print(f"El promedio total de {n_notas} es igual a:{promedio_n_notas}")
+        promedio_n_notas= sum(numeros)/len(numeros)
+        print(f"El promedio total de: {n_notas} es igual a:{promedio_n_notas}")
 
 def numero_mayor_menor (numeros): #Ingresar n números y mostrar el mayor y el menor
     if not numeros:
@@ -45,6 +45,7 @@ def numero_mayor_menor (numeros): #Ingresar n números y mostrar el mayor y el m
     else:
         mayor = max(numeros)
         minimo = min(numeros)
+
 bienvenida()
 
 def menu():
@@ -55,22 +56,25 @@ def menu():
 def menu2():
     print("_____Menú de opcione_____")
     print("1. Ingresar n numeros y mostrar:")
-        print("1. la suma total")
-        print("2. El promedio")
-        print("3. La cantidad de numeros positivos y negativos")
     print("2. Calcular el area de un triangulo:")
     print("3. Verificar si un numero es par o impar")
     print("4. Calcular el promedio de n calificaciones: ")
     print("5. Ingresar n numeros y mostrar el mayor y el menor")
     print("6. Salir del programa")
 
+notas =[]
+numeros=[]
+
 
 while True:
-    print (menu2)
-    opcion= int(input("Elija su opcion (1-6): "))
 
-    match case:
+    print (menu2())
+    opcion= input("=====Elija su opcion (1-6)=====: ")
+    match opcion:
         case "1":
+            while True:
+                
+    break
             
 
 
