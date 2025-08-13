e2f8b790c82fe8a8fa13eba8434ac1c32d47a936
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-08-12 19:11:45 -0600

    commit

--- Actividad_6.py
+++ Actividad_6.py
@@ -8,14 +8,17 @@ def suma_total(numeros): #funcion de suma total
 def promedio(numeros): #funcion del promedio de n numeros
     return sum(numeros)/ len(numeros)
 
+#funcion de un contador de numeros positivos y negativos
 def numeros_positivos_negativos (numeros):
     numeros_pos= 0
     numeros_nega= 0#funcion de conteo de numeros negativos y positivos
     for i in range(numeros):
         if i <0:
             numeros_nega +=1
+            return numeros_nega
         else:
             numeros_pos +=1
+            return numeros_pos
     print(f"TOTAL NUMEROS NEGATIVOS: {numeros_nega}")
     print(f"TOTAL NUMEROS POSITIVOS: {numeros_pos}")
 
@@ -23,7 +26,7 @@ def numeros_positivos_negativos (numeros):
 def area_triangulo (base,altura): #funcion calcular area de un triangulo
     return (base* altura) / 2
 
-def par_impar(numero): # funcion par o impar
+def par_impar(numero): # funcion numero par o impar
     if numero % 2 ==0:
         print(f"{numero} = es par")
     else:
@@ -33,9 +36,9 @@ def promedio_n_notas(numeros): #funcion calcular el promedio de n notas
     while True:
         n_notas=int(input(f"Calcular el promedio de n calificaciones: "))
         for i in range(n_notas):
-            notas=int(input(f"Ingrese el numero_{i+1}: "))
+            notas=int(input(f"Ingrese el valor para el numero_{i+1}: "))
             numeros.append(notas)
-            print("Numero guardado")
+            print("Numero guardado exitosamente")
         promedio_n_notas= sum(numeros)/len(numeros)
         print(f"El promedio total de: {n_notas} es igual a:{promedio_n_notas}")
 
@@ -69,10 +72,29 @@ numeros=[]
 while True:
 
     print (menu2())
-    opcion= input("=====Elija su opcion (1-6)=====: ")
-    match opcion:
+    op= input("=====Elija su opcion (1-6)=====: ")
+    match op:
         case "1":
+            numbers= []
             while True:
+                numeros2=input("__________Cuantos numeros desea ingresar: __________")
+                for i in range(numeros2):
+                    cantidad_numeros= int(input(f"__Ingrese el valor para el numero {i+1}__: "))
+                    numbers.append(cantidad_numeros)
+                    print(f"NUMERO GUARDADO EXITOSAMENTE")
+            print(menu())
+            op2=input("____Selecciona una opcion (1-3)___: ")
+            match op2:
+                case "1":
+                    numbers_sum= sum(numbers)
+                    print(f"===LA SUMA TOTAL DE {numbers}, ES IGUAL A: {numbers_sum}")
+
+                case "2":
+                    promedio1= promedio(numbers)
+
+
+
+
                 
     break
             
