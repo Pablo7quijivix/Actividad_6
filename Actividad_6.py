8fa048b575c39cf1a2828bbd70405179989d206c
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-08-12 19:57:13 -0600

    COMMIT ULTIMOS AJUESTES

--- Actividad_6.py
+++ Actividad_6.py
@@ -9,7 +9,7 @@ def promedio(numbers): #funcion del promedio de n numeros
     return sum(numbers)/ len(numbers)
 
 #funcion de un contador de numeros positivos y negativos
-def numeros_positivos_negativos (numbers):
+def num_pos_nega(numbers):
     numeros_pos= 0
     numeros_nega= 0#funcion de conteo de numeros negativos y positivos
     for i in range(numbers):
@@ -58,10 +58,10 @@ def menu():
 
 def menu2():
     print("_____Menú de opcione_____")
-    print("1. Ingresar n numeros y mostrar:")
-    print("2. Calcular el area de un triangulo:")
+    print("1. Ingresar n numeros y mostrar")
+    print("2. Calcular el area de un triangulo")
     print("3. Verificar si un numero es par o impar")
-    print("4. Calcular el promedio de n calificaciones: ")
+    print("4. Calcular el promedio de n calificaciones ")
     print("5. Ingresar n numeros y mostrar el mayor y el menor")
     print("6. Salir del programa")
 
@@ -77,30 +77,62 @@ while True:
         case "1":
             numbers= []
             while True:
-                numeros2=input("__________Cuantos numeros desea ingresar: __________")
-                for i in range(numeros2):
-                    cantidad_numeros= int(input(f"__Ingrese el valor para el numero {i+1}__: "))
-                    numbers.append(cantidad_numeros)
-                    print(f"NUMERO GUARDADO EXITOSAMENTE")
-            print(menu())
-            op2=input("____Selecciona una opcion (1-3)___: ")
-            match op2:
-                case "1":
-                    numbers_sum= sum(numbers)
-                    print(f"===LA SUMA TOTAL DE {numbers}, ES IGUAL A: {numbers_sum}")
+                numeros2=int(input("__Cuantos numeros desea ingresar__(pulse 0 si ya no desaea agreagar numeros)________: "))
+                if numeros2 != 0:
+                    for i in range(numeros2):
+                        cantidad_numeros= int(input(f"__Ingrese el valor para el numero {i+1}__: "))
+                        numbers.append(cantidad_numeros)
+                        print(f"NUMERO GUARDADO EXITOSAMENTE")
+                else:
+                    break
+            while True:
+                print(menu())
+                op2 = input("____Selecciona una opcion (1-4)___: ")
+                match op2:
+                    case "1":
+                        numbers_sum = sum(numbers)
+                        print(f"===LA SUMA TOTAL DE {numbers}, ES IGUAL A: {numbers_sum}")
 
-                case "2":
-                    promedio1= promedio(numbers)
-                    print(f"El Promedio es igual a:____ {promedio1} ____")
+                    case "2":
+                        promedio1 = promedio()
+                        print(f"El Promedio es igual a:____ {promedio1} ____")
 
-                case "3":
-                    numbers_p_negative= numeros_positivos_negativos(numbers)
-                    print(numbers_p_negative)
+                    case "3":
+                        numbers_p_negative = num_pos_nega(numbers)
+                        print(numbers_p_negative)
+
+                    case "4":
+                        print(f"==SALIENDO DEL SUBMENU DE LA OPCION 1")
+                        break
+                    case _:
+                        print(f"===OPCION NO VALIDA, INTENTE DE NUEVO===")
+
 
         case "2":
             a=int(input("__INGRESE UN VALOR PARA LA ALTURA DEL TRIANGULO__: "))
-            b=
-            area_t= area_triangulo()
+            b=int(input("__INGRESE UN VALOR PARA LA BASE DEL TRIANGULO__: "))
+            area_t= area_triangulo(a,b)
+            print(area_t)
+            #le pasamos a los parametros los valores de a y b que estos numeros son la altura
+            # Y base de nuestro triangulo a calcular
+        case "3":
+            dato=int(input(f"___INGRESE UN NUMERO PARA DETERMINAR SI ES PAR O IMPAR__: "))
+            parimpar= par_impar(dato)
+            print(parimpar)
+
+        case "4":
+            print(promedio_n_notas(numeros))
+
+        case "5":
+            while True:
+                nums= input("--CUANTOS NUMEROS DESEA INGRESAR--: ")
+                for i in range(nums):
+                    nums2=int(input(f"--INGRESE EL VALOR PARA EL NUMERO {i+1}: "))
+                    numeros.append(nums2)
+                print(numero_mayor_menor(numeros))
+        case "6":
+            print("==========SALIENDO DEL PROGRAMA==========")
+
 
 
 
